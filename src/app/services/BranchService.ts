bbc4e1d5fc849e00630a2a1cbd8783caab15f271
staff1 <dff-staff@outlook.com>
2018-10-17 20:02:44 +0530

    creating branch controller and branch service

--- src/app/services/BranchService.ts
+++ src/app/services/BranchService.ts
@@ -1,26 +1,26 @@
 import { App } from "../../utils/App";
-import { ConsumerDAO } from "../repos/ConsumerDAO";
+import { BranchDAO } from "../repos/BranchDAO";
 import { AddressDAO } from "../repos/AddressDAO";
 import { ImgDAO } from "../repos/ImgDAO";
-import { Consumer } from "../../entities/Consumer";
+import { Branch } from "../../entities/Branch";
 import { Props } from "../../utils/Props";
 import { hashSync, compareSync } from "bcryptjs";
 
-export class ConsumerService {
+export class BranchService {
   public sessionInfo: any;
-  private consumerDao: ConsumerDAO;
+  private branchDao: BranchDAO;
   private addressDao: AddressDAO;
   private imgDao: ImgDAO;
 
   constructor() {
-    this.consumerDao = new ConsumerDAO();
+    this.branchDao = new BranchDAO();
     this.addressDao = new AddressDAO();
     this.imgDao = new ImgDAO();
   }
 
   async entity(id: string) {
     try {
-      let data: any = await this.consumerDao.entity(id);
+      let data: any = await this.branchDao.entity(id);
       console.log(data);
       return data;
     } catch (error) {
@@ -31,29 +31,23 @@ export class ConsumerService {
   async search(reqData: any) {
     try {
       reqData.vid = this.sessionInfo.vid;
-      let data: any = await this.consumerDao.search(reqData);
+      let data: any = await this.branchDao.search(reqData);
       return data;
     } catch (error) {
       throw error;
     }
   }
 
-  async validate(item: Consumer) {
-    // if (!item.id || item.id == "" || item.id == "0") {
-    //   let uid = App.UniqueNumber();
-    //   item.id = uid;
-    // } else {
-    //   item.vid = this.sessionInfo.vid;
-    //   return true;
-    // }
-
+  async validate(item: Branch) {
     if (!item.id || item.id == "" || item.id == "0") {
       item.id = null;
     }
     item.updatedBy = this.sessionInfo.id;
     let query: {};
-    let data = await this.consumerDao.search({ email: item.email });
-    let mdata = await this.consumerDao.search({ mobile: item.mobile });
+    console.log(item);
+    let data = await this.branchDao.search({ email: item.email });
+    let mdata = await this.branchDao.search({ mobile: item.mobile });
+    console.log(data);
     if ((item.id && data.length > 1) || (!item.id && data.length > 0)) {
       return "Email";
     } else if ((item.id && mdata.length > 1) || (!item.id && mdata.length > 0)) {
@@ -70,14 +64,13 @@ export class ConsumerService {
     }
   }
 
-  async save(item: Consumer) {
-    try {
+  async save(item: Branch) {
+     try {
       let cond = await this.validate(item);
       if (cond == true) {
         let addressData: any = await this.addressDao.save(item.address);
         let imgData: any = await this.imgDao.save(item.img);
-        // item.password = hashSync(item.password, 8);
-        let profileData: any = await this.consumerDao.save(item);
+        let profileData: any = await this.branchDao.save(item);
         let returnData = {
           id: item.id,
           message: Props.SAVED_SUCCESSFULLY
