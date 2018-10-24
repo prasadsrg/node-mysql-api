0b3c5f91e58e5d8538af1f41b0599bae44be3032
dff-staff <dff-staff@outlook.com>
2018-10-24 20:52:08 +0530

    commit for consumer

--- src/app/services/ConsumerService.ts
+++ src/app/services/ConsumerService.ts
@@ -5,6 +5,8 @@ import { ImgDAO } from "../repos/ImgDAO";
 import { Consumer } from "../../entities/Consumer";
 import { Props } from "../../utils/Props";
 import { hashSync, compareSync } from "bcryptjs";
+import { Img } from "../../entities/Img";
+import { Address } from "../../entities/Address";
 
 export class ConsumerService {
   public sessionInfo: any;
@@ -51,28 +53,40 @@ export class ConsumerService {
       item.id = null;
     }
     item.updatedBy = this.sessionInfo.id;
+    console.log(item);
     let query: {};
     let data = await this.consumerDao.search({ email: item.email });
     let mdata = await this.consumerDao.search({ mobile: item.mobile });
     if ((item.id && data.length > 1) || (!item.id && data.length > 0)) {
+      console.log("Email");
       return "Email";
     } else if ((item.id && mdata.length > 1) || (!item.id && mdata.length > 0)) {
+      console.log("Mobile");
       return "Mobile";
     } else {
+      console.log("else");
       if (!item.id) {
         let uid = App.UniqueNumber();
+        console.log(item.id);
         item.id = uid;
+        if (!item.address) {
+          item.address = new Address();
+        }
         item.address.id = uid;
+        if (!item.img) {
+          item.img = new Img();
+        }
         item.img.id = uid;
         item.vid = this.sessionInfo.vid;
       }
-      return true;
     }
+    return true;
   }
 
   async save(item: Consumer) {
     try {
       let cond = await this.validate(item);
+      console.log(cond);
       if (cond == true) {
         let addressData: any = await this.addressDao.save(item.address);
         let imgData: any = await this.imgDao.save(item.img);
@@ -82,17 +96,16 @@ export class ConsumerService {
           id: item.id,
           message: Props.SAVED_SUCCESSFULLY
         };
+        console.log(returnData);
         return returnData;
       } else if (cond == "Email") {
-        let returnData = {
+        throw {
           message: Props.EMAIL_EXISTS
         };
-        return returnData;
       } else if (cond == "Mobile") {
-        let returnData = {
+        throw {
           message: Props.MOBILE_EXISTS
         };
-        return returnData;
       }
     } catch (error) {
       return error;
